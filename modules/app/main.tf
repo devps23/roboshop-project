21213483a43001e335ac18485c3d2f879af0e03c
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-02-03 11:02:04 +0530

    bastion nodes

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "component" {
   ami = data.aws_ami.ami.image_id
   instance_type = var.instance_type
   subnet_id = var.subnets[0]
-  vpc_security_group_ids = [aws_security_group.security_group.id]
+
   instance_market_options {
     market_type = "spot"
     spot_options {
