625cdca3edf04e9817eebced3d424213e5deb897
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-02-05 11:37:21 +0530

    bastion nodes and zone id

--- modules/rabbitmq/main.tf
+++ modules/rabbitmq/main.tf
@@ -20,6 +20,9 @@ resource "aws_security_group" "security_group" {
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  tags = {
+    Name = "${var.component}-${var.env}-sg"
+  }
 }
 resource "aws_instance" "instance" {
   ami                    = data.aws_ami.ami.id
