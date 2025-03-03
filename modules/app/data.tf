9933242ce662a00bfc6fe64c0334f9aa434b7441
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-03-03 15:57:22 +0530

    centos

--- modules/app/data.tf
+++ modules/app/data.tf
@@ -3,3 +3,16 @@ data "aws_ami" "ami" {
   name_regex       = "RHEL-9-DevOps-Practice"
   owners           = [973714476881]
 }
+
+# centos ami
+# ami-0b4f379183e5706b9
+# Owner: 973714476881
+
+# OwnerAlias: –
+# Platform: –
+# Architecture: x86_64
+#
+# Publish date: 2024-06-05
+# Root device type: ebs
+# Virtualization: hvm
+# ENA enabled: Yes
